e7ea0e7961945f5cf11750821412f70efa5c380f
“Erin <erinkelley27@gmail.com>
2018-07-30 11:39:00 -0400

    added a burger in seed.js

--- db/seed.js
+++ db/seed.js
@@ -7,11 +7,32 @@ const createPassword = password =>
 
 User.find({}).remove(() => {
   Burger.find({}).remove(() => {
-    let erin = User.create({
+    let admin = User.create({
       local: {
         email: 'erinkelley27@gmail.com',
         password: createPassword('password1')
       }
+    }).then(user => {
+      Promise.all([
+        Burger.create({
+          image: './public/images/default-burger',
+          name: 'Breakfast Burger',
+          description: 'When breakfast is on a burger, you can eat burgers any time!',
+          toppings: {
+            bun: 'Brioche Bun',
+            meat: 'Ground Beef Patty',
+            topping1: 'Maple & Black Pepper Candied Bacon',
+            topping2: 'Runny Egg',
+            topping3: 'Sharp Cheddar Cheese',
+            topping4: ' ',
+            topping5: ' '
+          }
+        }).then(burger => {
+          user.burgers.push(burger)
+        })
+      ]).then(() => {
+        user.save(err => console.log(err))
+      })
     })
   })
 })
