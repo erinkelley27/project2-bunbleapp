e6638de95d281249364de38c283f017bc16cfc09
“Erin <erinkelley27@gmail.com>
2018-07-30 13:22:22 -0400

    updated server with hbs parials and a route requirement

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+// What is this?
+router.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
+
 router.use('/', require('./index.js'))
 router.use('/user', require('./user.js'))
 router.use('/burger', require('./burger.js'))
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const hbs = require('hbs')
 
+// Does it matter what order these are in?
 const app = express()
+hbs.registerPartials(_dirname + 'view/partials')
+app.use(express.static('public'))
 app.set('view engine', 'hbs')
 
-app.use(require('./routes/index.js'))
+app.use(require('./routes/app.js'))
 
 app.listen(3000, () => console.log('running on port 3000'))
