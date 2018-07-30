320e53be91ee12b2784a21292aa407bbdd5fcc09
“Erin <erinkelley27@gmail.com>
2018-07-30 13:14:47 -0400

    app.use route in server

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const hbs = require('hbs')
 const app = express()
 app.set('view engine', 'hbs')
 
+app.use(require('./routes/index.js'))
+
 app.listen(3000, () => console.log('running on port 3000'))
