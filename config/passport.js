d13862f691e808c81eb63c71c7629055aba4211b
“Erin <erinkelley27@gmail.com>
2018-08-01 09:54:20 -0400

    declared stategy for signup

---
+++ config/passport.js
@@ -0,0 +1,12 @@
+const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+
+module.exports = function (passport) {
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function (req, email, password, callback) {
+
+  }))
+}
--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const userController = require('../controllers/user.js')
 
 router.get('/user/:id', userController.show)
-// router.get('/signup', userController.signUp)
-// router.post('/signup', userController.createSignUp)
+router.get('/signup', userController.signUp)
+router.post('/signup', userController.createSignUp)
 
 // router.get('/login', userController.login)
 // router.post('/login', userController.createLogin)
