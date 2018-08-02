f666554c0130b3e83dab97a2d94b7cb9a8fc7d0d
“Erin <erinkelley27@gmail.com>
2018-08-02 11:27:40 -0400

    update is working for commets

--- controllers/burger.js
+++ controllers/burger.js
@@ -3,12 +3,8 @@ const { Burger, Comment } = require('../models/Burger')
 module.exports = {
   show: (req, res) => {
     Burger.findOne({ _id: req.params.id })
-      .exec(function (err, burger) {
-        Comment.populate(burger.comments, { path: 'author' },
-          function (err, comments) {
-            burger.comments = comments
-            res.render('burger/show', { burger })
-          })
+      .then(burger => {
+        res.render('burger/show', { burger })
       })
   },
   update: (req, res) => {
