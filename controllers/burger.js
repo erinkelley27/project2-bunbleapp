a5f1dec0825cab81d4f20d5d8d21e34c16e0f1d0
“Erin <erinkelley27@gmail.com>
2018-08-02 13:38:28 -0400

    delete controller attempt 1

--- controllers/burger.js
+++ controllers/burger.js
@@ -16,5 +16,11 @@ module.exports = {
           res.redirect(`/burger/${burger._id}`)
         })
       })
+  },
+  delete: (req, res) => {
+    Comment.findOneAndRemove({ _id: req.params.id })
+    .then(burger => {
+      res.redirect('/burger/:id')
+    })
   }
 }
