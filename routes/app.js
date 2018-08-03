15daa14e2d2e7020a7af84e05584cfcea367a5bc
“Erin <erinkelley27@gmail.com>
2018-08-03 11:17:38 -0400

    changed controller caps to lower case

--- routes/app.js
+++ routes/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const indexController = require('../controllers/Index.js')
+const indexController = require('../controllers/index.js')
 
 router.get('/', indexController.index)
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../controllers/User.js')
+const userController = require('../controllers/user.js')
 
 router.get('/user/:id', userController.show)
 router.get('/new', userController.new)
