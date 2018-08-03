b2678a12c1c1a6868f49d23f9300b01c2f1e9add
“Erin <erinkelley27@gmail.com>
2018-08-03 11:26:08 -0400

    moved some things around

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-// const express = require('express')
-// const router = express.Router()
+const express = require('express')
+const router = express.Router()
 
 // What is this? AUTHENTICATION
-// router.use((req, res, next) => {
-//   res.locals.currentUser = req.user
-//   next()
-// })
+router.use((req, res, next) => {
+  res.locals.currentUser = req.user
+  next()
+})
 
 // What are these requirements? ROUTES
-// router.use('/', require('./index.js'))
-// router.use('/user', require('./user.js'))
-// router.use('/burger', require('./burger.js'))
+router.use('/', require('./index.js'))
+router.use('/user', require('./user.js'))
+router.use('/burger', require('./burger.js'))
 
-// router.all('*', (req, res) => {
-//   res.status(400).send()
-// })
+router.all('*', (req, res) => {
+  res.status(400).send()
+})
 
-// module.exports = router
+module.exports = router
