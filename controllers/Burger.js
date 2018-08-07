123723faf9ba19a7586daa152f22702ba2685e43
“Erin <erinkelley27@gmail.com>
2018-08-06 21:30:16 -0400

    image css for id partial

--- controllers/Burger.js
+++ controllers/Burger.js
@@ -16,14 +16,17 @@ module.exports = {
           res.redirect(`/burger/${burger._id}`)
         })
       })
-  },
-  removeComment: (req, res) => {
-    Burger.findOne({ _id: req.params.id })
-      .then(burger => {
-        burger.update({$pull: { comments: { _id: req.body.id }}})
-        burger.save(err => {
-          res.redirect(`/burger/${burger._id}`)
-        })
-      })
   }
+  // removeComment: (req, res) => {
+  //   Burger.findOne({ _id: req.params.id })
+  //     .then(burger => {
+  //       burger.update({$pull: { comments: { _id: req.body.id }}})
+  //       burger.save(err => {
+  //         res.redirect(`/burger/${burger._id}`)
+  //       })
+  //     })
+  // },
+  // deleteComment: (burgerId, commentId) => {
+  //   let comments = Burger.findOne({id: burgerId})
+  // }
 }
